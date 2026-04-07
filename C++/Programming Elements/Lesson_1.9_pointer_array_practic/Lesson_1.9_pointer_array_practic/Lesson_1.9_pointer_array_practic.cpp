a615bb486e021b20933e32d17fc7fd678ba9ed82
AntonShkola <anton.shkola@yandex.ru>
2026-04-07 22:02:01 +0300

    main написал код

--- C++/Programming Elements/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic.cpp
+++ C++/Programming Elements/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic.cpp
@@ -1,6 +1,13 @@
-﻿#include <iostream>
+﻿/*void function(int*& pVariable)
+{
 
-using namespace std;
+	*pVariable = 800;
+
+	cout << pVariable << endl;
+
+	pVariable = nullptr;
+
+}
 
 int main()
 {
@@ -16,7 +23,41 @@ int main()
 	
 	*ptrVariable = 300;
 
-	cout << variable << endl;
+	function(ptrVariable);
 
+	cout << variable << endl;
+	cout << ptrVariable << endl;
+
+	return 0;
+} */
+
+#include <iostream>
+
+using namespace std;
+
+void printArray(int* integers, int size)
+{
+	for (int counter{}; counter < size; ++counter)
+	{
+		cout << integers[counter] << " ";
+	}
+
+}
+
+int main()
+{
+
+	int variable{ 100 };
+	int* pVariable{ &variable }; // &int -> int*
+	int** ppVariable{ &pVariable };
+	int*** pppVariable{ &ppVariable };
+
+	//cout << **pppVariable << endl;
+
+	const int size{ 5 };
+	int integers[size]{ 1,2,3,4,5 };
+	printArray(integers,size);
+
+	
 	return 0;
 }
