8e7e9a653c6ab3c62cbc9b23993e266f6f2579e9
AntonShkola <anton.shkola@yandex.ru>
2026-04-07 07:42:42 +0300

    pointer practice

---
+++ C++/Programming Elements/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int variable{ 100 };
+	int* ptrVariable{ &variable };
+
+	cout << variable << endl;
+	cout << &variable << endl;
+	cout << ptrVariable << endl;
+	cout << &ptrVariable << endl;
+	cout << *ptrVariable << endl;
+	
+	*ptrVariable = 3000;
+
+	cout << variable << endl;
+
+	return 0;
+}
