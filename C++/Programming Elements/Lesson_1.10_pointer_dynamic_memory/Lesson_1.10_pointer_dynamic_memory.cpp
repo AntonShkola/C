b8ecf18a451ebb2a1936335f563a1b49da2cf9f5
AntonShkola23 <a.shkola@carx-tech.com>
2026-04-08 13:49:07 +0300

    dynamic memory

---
+++ C++/Programming Elements/Lesson_1.10_pointer_dynamic_memory/Lesson_1.10_pointer_dynamic_memory.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+	const int arrayElements{ 1 }; // переменная константа с числом элементов массива
+	int arraySize{ arrayElements * sizeof(int) }; // количество байт, которое нужно выделить в динамической памяти
+	int* integers{ static_cast<int*>(malloc(arraySize)) }; // выделение в памяти нужного количества байт
+
+	if (integers) // проверяем не является ли указатель 0: if (integers != nullptr)
+	{
+
+	}
+
+	return 0;
+}
