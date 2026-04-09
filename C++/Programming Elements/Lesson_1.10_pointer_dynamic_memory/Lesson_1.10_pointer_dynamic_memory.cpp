8ac65d6e0707ee4593b7713953d2070db53adf65
AntonShkola23 <a.shkola@carx-tech.com>
2026-04-09 13:45:04 +0300

    dynamic memory malloc and free

--- C++/Programming Elements/Lesson_1.10_pointer_dynamic_memory/Lesson_1.10_pointer_dynamic_memory.cpp
+++ C++/Programming Elements/Lesson_1.10_pointer_dynamic_memory/Lesson_1.10_pointer_dynamic_memory.cpp
@@ -1,14 +1,20 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 int main()
 {
 	const int arrayElements{ 1 }; // переменная константа с числом элементов массива
 	int arraySize{ arrayElements * sizeof(int) }; // количество байт, которое нужно выделить в динамической памяти
 	int* integers{ static_cast<int*>(malloc(arraySize)) }; // выделение в памяти нужного количества байт
 
+	//cout << integers << endl;
+
 	if (integers) // проверяем не является ли указатель 0: if (integers != nullptr)
 	{
-
+		integers[0] = 200;
+		cout << integers[0] << endl;
+		free(integers);
 	}
 
 	return 0;
