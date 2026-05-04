a6c804646e5f52cde42a3113c0c6aa11a2604277
AntonShkola23 <a.shkola@carx-tech.com>
2026-05-04 18:50:11 +0300

    pointer repeat practice

--- C++/Programming Elements/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic.cpp
+++ C++/Programming Elements/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic.cpp
@@ -1,63 +1,11 @@
-﻿/*void function(int*& pVariable)
-{
-
-	*pVariable = 800;
-
-	cout << pVariable << endl;
-
-	pVariable = nullptr;
-
-}
-
-int main()
-{
-
-	int variable{ 100 };
-	int* ptrVariable{ &variable };
-
-	cout << variable << endl;
-	cout << &variable << endl;
-	cout << ptrVariable << endl;
-	cout << &ptrVariable << endl;
-	cout << *ptrVariable << endl;
-	
-	*ptrVariable = 300;
-
-	function(ptrVariable);
-
-	cout << variable << endl;
-	cout << ptrVariable << endl;
-
-	return 0;
-} */
-
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
-void printArray(int* integers, int size)
-{
-	for (int counter{}; counter < size; ++counter)
-	{
-		cout << integers[counter] << " ";
-	}
-
-}
-
 int main()
 {
+	int number = 5;
+	cout << typeid(&number).name();
 
-	int variable{ 100 };
-	int* pVariable{ &variable }; // &int -> int*
-	int** ppVariable{ &pVariable };
-	int*** pppVariable{ &ppVariable };
-
-	//cout << **pppVariable << endl;
-
-	const int size{ 5 };
-	int integers[size]{ 1,2,3,4,5 };
-	printArray(integers,size);
-
-	
 	return 0;
 }
