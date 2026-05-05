b66b914b15daee8ad4fb1682ca19917b7f2b3536
AntonShkola23 <a.shkola@carx-tech.com>
2026-05-05 18:47:16 +0300

    variable to function

--- C++/Programming Elements/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic.cpp
+++ C++/Programming Elements/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic.cpp
@@ -2,10 +2,47 @@
 
 using namespace std;
 
+void incrementPtr(double* pointer)
+{
+	*pointer = *pointer + 1.5;
+}
+
+void incrementValue(int number)
+{
+	number = number + 5;
+	cout << number << " : Local value" << endl;
+}
+
+void incrementRef(float& b)
+{
+	b++;
+}
+
 int main()
 {
-	int number = 5;
-	cout << typeid(&number).name();
+	/// Передача через указаатель
+
+	double speed = 70.5;
+	cout << speed << " : Value before change" << endl;
+
+	incrementPtr(&speed);
+	cout << speed << " : Value after change" << endl;
+	
+	/// Передача через значение
+
+	int a = 10;
+	cout << a << " : Value before function call" << endl;
+
+	incrementValue(a);
+	cout << a << " : Value after funcrion call" << endl;
+
+	/// Передача через ссылку
+
+	float z = 0.35;
+	cout << z << " : Value before change" << endl;
+
+	incrementRef(z);
+	cout << z << " : Value after change" << endl;
 
 	return 0;
 }
