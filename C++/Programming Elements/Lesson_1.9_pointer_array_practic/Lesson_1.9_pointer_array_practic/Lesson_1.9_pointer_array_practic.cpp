1ada23bd6e66f9e5e9d633319f14f11a4f15a9a3
AntonShkola23 <a.shkola@carx-tech.com>
2026-05-06 18:47:56 +0300

    massive practice

--- C++/Programming Elements/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic.cpp
+++ C++/Programming Elements/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic/Lesson_1.9_pointer_array_practic.cpp
@@ -10,7 +10,7 @@ void incrementPtr(double* pointer)
 void incrementValue(int number)
 {
 	number = number + 5;
-	cout << number << " : Local value" << endl;
+	//cout << number << " : Local value" << endl;
 }
 
 void incrementRef(float& b)
@@ -18,31 +18,50 @@ void incrementRef(float& b)
 	b++;
 }
 
+void incrementArr(int* numbers, int size)
+{
+
+	cout << numbers << endl;
+
+	for (int i = 0; i <= 2; i++)
+	{
+		cout << i << endl;
+		cout << &numbers[i] << endl;
+	}
+
+	cout << typeid(size).name() << endl;
+}
+
 int main()
 {
 	/// Передача через указаатель
 
 	double speed = 70.5;
-	cout << speed << " : Value before change" << endl;
+	//cout << speed << " : Value before change" << endl;
 
 	incrementPtr(&speed);
-	cout << speed << " : Value after change" << endl;
+	//cout << speed << " : Value after change" << endl;
 	
 	/// Передача через значение
 
 	int a = 10;
-	cout << a << " : Value before function call" << endl;
+	//cout << a << " : Value before function call" << endl;
 
 	incrementValue(a);
-	cout << a << " : Value after funcrion call" << endl;
+	//cout << a << " : Value after funcrion call" << endl;
 
 	/// Передача через ссылку
 
 	float z = 0.35;
-	cout << z << " : Value before change" << endl;
+	//cout << z << " : Value before change" << endl;
 
 	incrementRef(z);
-	cout << z << " : Value after change" << endl;
+	//cout << z << " : Value after change" << endl;
+
+	// Передача массива
+
+	int array[] = { 5,7,9 };
+	incrementArr(array, 3);
 
 	return 0;
 }
